97e46705d82d524a7c04dc06d801329d79a44da5
Lunadd-00 <luna.dd26@gmail.com>
2026-01-29 18:56:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lunadd-tienda
+Este será el repositorio para el proyecto del curso de Desarrollo Web y Patrones
---
+++ src/main/java/com/techShop/tienda/ProjectConfig.java
@@ -0,0 +1,37 @@
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+/**
+ *
+ * @author lunad
+ */
+public class ProjectConfig implements WebMvcConfigurer {
+
+    // prueba ruta correcta
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/iframes").setViewName("iframes");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+
+}
