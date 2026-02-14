f1de0159378e5fef99f3a034ce572e53618ad6ae
Lunadd-00 <luna.dd26@gmail.com>
2026-02-13 22:32:11 -0600

    Laboratorio4

---
+++ src/main/java/com/techShop/tienda/repository/ProductoRepository.java
@@ -0,0 +1,15 @@
+package com.techShop.tienda.repository;
+
+import com.techShop.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author lunad
+ */
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    public List<Producto> findByActivoTrue();
+
+}
